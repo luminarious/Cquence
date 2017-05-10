344a109cda1ef1584894d3acdadfd9cf59744fbd
Hardi Kõvamees <hardikovamees@l26840.meie.politsei.ee>
2017-05-10 10:39:03 +0300

    Convert source to ES6

--- Cquence.js
+++ Cquence.js
@@ -1,165 +1,149 @@
+'use strict'
 
-var Cq;
+const Cq = (() => {
+	const raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || ((f) => { setTimeout(f, 1000 / 60) })
+	const elem = (id) => document.getElementById(id)
 
-Cq = function(){
-    var raf = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function( f ){ setTimeout( f, 1000/60 ); };
-    var elem = function( id ){ return document.getElementById( id ); };
+	// IE checking
+	const IEVERSION = getInternetExplorerVersion()
+	const IE8 = IEVERSION === 8
+	const IEWTF = IEVERSION < 8 && IEVERSION > -1
 
-    // IE checking
-    var IEVERSION = getInternetExplorerVersion();
-    var IE8 = IEVERSION === 8;
-    var IEWTF = IEVERSION < 8 && IEVERSION > -1;
+	const style = (e, k, v) => {
+		if (k === 'opacity') {
+			if (IE8) {
+				e.style['-ms-filter'] = `progid:DXImageTransform.Microsoft.Alpha(Opacity=${Math.floor(v * 100)})`
+				e.style.filter = `alpha(opacity=${Math.floor(v * 100)})`
+			} else {
+				e.style[k] = v
+			}
+		} else {
+			e.style[k] = `${v}px`
+		}
+	}
 
-    var style = function( e, k, v ){
-        if( k === 'opacity' ){
-          if( IE8 ){ // gemene browsers
-            e.style[ "-ms-filter" ] = "progid:DXImageTransform.Microsoft.Alpha(Opacity=" + Math.floor( v * 100 ) + ")";
-            e.style.filter = "alpha(opacity=" + Math.floor( v * 100 ) + ")";
+	const combine = () => {
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope/arguments
+		const list = Array.prototype.slice.call(arguments, 0)
+		let d = 0
+		for (let i = 0; i < list.length; i++) {
+			d = Math.max(list[i].d, d)
+		}
+		return {
+			d,
+			f(t) {
+				for (const last of list) {
+					if (last.d > t) {
+						last.f(t)
+					} else if (!last.done) {
+						last.f(last.d)
+						last.done = true
+					}
+				}
+			}
+		}
+	}
 
-          } else { // lieve browsers
-            e.style[k] = v;
-          }
-        } else {
-          e.style[k] = v + "px";
-        }
-    };
+	const sequence = () => {
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope/arguments
+		const timeline = Array.prototype.slice.call(arguments, 0)
+		let d = 0
+		for (let i = 0; i < timeline.length; i++) {
+			d += timeline[i].d
+		}
+		return {
+			d,
+			f(t) {
+				let last = null
+				let total = 0
 
-    var combine = function(){
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope/arguments
-        var list = Array.prototype.slice.call( arguments, 0 );
-        var d = 0;
-        for( var i=0; i<list.length; i++ ){
-            d = Math.max( list[i].d, d );
-        }
-        return {
-            d: d,
-            f: function( t ){
-                for( var i=0; i<list.length; i++ ){
-                    var last = list[i];
-                    if( last.d > t ){
-                        last.f( t );
-                    } else {
-                      if( !last.done ){
-                          last.f( last.d );
-                          last.done = true;
-                      }
-                    }
+				for (let i = 0; i < timeline.length; i++) {
+					last = timeline[i]
+					if (total + last.d > t) {
+						last.f(t - total)
+						return
+					}
+					if (!last.done) {
+						last.f(last.d)
+						last.done = true
+					}
+					total += last.d
+				}
+			}
+		}
+	}
 
-                }
-            }
-        };
-    };
+	const animate = (transform) => (id, dur, begin, fin) => ({
+		d: dur,
 
-    var sequence = function(){
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions_and_function_scope/arguments
-        var timeline = Array.prototype.slice.call( arguments, 0 );
-        var d = 0;
-        for( var i=0; i<timeline.length; i++ ){
-            d = d + timeline[i].d;
-        }
-        return {
-            d: d,
-            f: function( t ){
+		f(t) {
+			const e = elem(id)
+			for (const k in begin) {
+				const bv = begin[k] // Begin waarde
+				const fv = fin[k]   // Finish waarde
+				const dx = fv - bv  // Verschil (afstand)
 
-                var last = null;
-                var total = 0;
+				// p is nu een waarde tussen de 0 en 1 .. en geeft aan hoe ver de animatie is
+				const p = transform(Math.max(t / dur, 0))
 
-                for( var i=0; i<timeline.length; i++ ){
-                    last = timeline[i];
-                    if( total + last.d > t ){
-                        last.f( t - total );
-                        return;
-                    }
-                    if( !last.done ){
-                        last.f( last.d );
-                        last.done = true;
-                    }
-                    total = total + last.d;
-                }
-            }
-        };
-    };
+				// Nieuwe waarde is de factor maal de afstand
+				const nv = bv + (p * dx)
+				style(e, k, nv)
+			}
+		}
+	})
 
-    var animate = function( transform ){
-        return function( id, dur, begin, fin ){
-            return {
-                d: dur,
-                f: function( t ){
-                    var e = elem( id );
-                    for( var k in begin ){
+	// Animation variants
+	// http://upshots.org/actionscript/jsas-understanding-easing
+	const linear = animate((p) => p)
+	const easeIn = animate((p) => p ** 5)
+	const easeOut = animate((p) => 1 - (1 - p) ** 5)
+	const sleep = (d) => ({
+		d,
+		f(t) {}
+	})
 
-                        var bv = begin[k]; // begin waarde
-                        var fv = fin[k];   // finish waarde
-                        var dx = fv - bv;  // verschil (afstand)
+	/* From: http://stackoverflow.com/questions/10964966/detect-ie-version-in-javascript */
+	// Returns the version of Internet Explorer or a -1
+	// (indicating the use of another browser).
+	function getInternetExplorerVersion() {
+		let rv = -1 // Return value assumes failure.
+		if (navigator.appName === 'Microsoft Internet Explorer') {
+			const re = new RegExp('MSIE ([0-9]{1,}[.0-9]{0,})')
+			if (re.exec(navigator.userAgent) !== null) {
+				rv = parseFloat(RegExp.$1)
+			}
+		}
+		return rv
+	}
 
-                        // p is nu een waarde tussen de 0 en 1 .. en geeft aan hoe ver de animatie is
-                        var p = transform( Math.max( t/dur, 0 ) );
+	const timeline = []
+	const start = (Number(new Date())) // Unix timestamp
+	const second = 1000
+	const stop = 15 * second
+	const render = null
 
-                        // nieuwe waarde is de factor maal de afstand
-                        var nv = bv + (p * dx);
-                        style( e, k, nv );
-                    }
-                }
-            };
-        };
-    };
+	// Gets called recursive by request-animation-frame
+	const renderloop = () => {
+		// Console.log('1. ', window.render );
+		const now = (Number(new Date())) - start      // Relatieve tijd vanaf begin animatie
+		if (now < stop) {
+			raf(renderloop)
+		}
+		// Console.log('2. ', window.render );
+		if (window.render) {
+			window.render.f(now)
+		}
+	}
 
-    // animation variants
-    // http://upshots.org/actionscript/jsas-understanding-easing
-    var linear = animate( function( p ){
-        return p;
-    });
-    var easeIn = animate( function( p ){
-        return Math.pow( p, 5 );
-    });
-    var easeOut = animate( function( p ){
-        return 1 - Math.pow( 1 - p, 5 );
-    });
-    var sleep = function( d ){
-        return { d: d, f: function(t){} };
-    };
-
-    /* from: http://stackoverflow.com/questions/10964966/detect-ie-version-in-javascript */
-    function getInternetExplorerVersion()
-    // Returns the version of Internet Explorer or a -1
-    // (indicating the use of another browser).
-    {
-        var rv = -1; // Return value assumes failure.
-        if (navigator.appName == 'Microsoft Internet Explorer')
-        {
-            var ua = navigator.userAgent;
-            var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-            if (re.exec(ua) !== null)
-                rv = parseFloat( RegExp.$1 );
-        }
-        return rv;
-    }
-
-    var timeline = [];
-    var start = (+new Date()); // unix timestamp
-    var second = 1000;
-    var stop = 15 * second;
-    var render = null;
-
-    // gets called recursive by request-animation-frame
-    var renderloop = function(){
-        // console.log('1. ', window.render );
-        var now = (+new Date()) - start;      // relatieve tijd vanaf begin animatie
-        if( now < stop ) raf( renderloop ); // recursion, baby
-        // console.log('2. ', window.render );
-        if( window.render ) window.render.f( now );
-
-    };
-
-    return {
-        combine: combine,
-        sequence: sequence,
-        linear: linear,
-        animate: animate,
-        easeIn: easeIn,
-        easeOut: easeOut,
-        sleep: sleep,
-        renderloop: renderloop
-    };
-
-}();
+	return {
+		combine,
+		sequence,
+		linear,
+		animate,
+		easeIn,
+		easeOut,
+		sleep,
+		renderloop
+	}
+})()
